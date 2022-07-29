99fd3221d7d76f281df95d7571460629addcae64
MaximKn1 <maximkn101@gmail.com>
2022-07-30 02:38:07 +0300

    Expanding test coverage and adding comments to test cases

--- src/tests/EmployeeDatabaseTest.java
+++ src/tests/EmployeeDatabaseTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Assert;
@@ -9,30 +10,41 @@ import org.junit.Test;
 
 import app.Employee;
 import app.EmployeeDatabase;
+import app.NullEmployee;
 
 public class EmployeeDatabaseTest {
 
 	@Test
 	public void testEmployeeDatabase() {
+		// Creating an empty database
 		EmployeeDatabase database = new EmployeeDatabase();
 		Assert.assertTrue(database.getDataBase().isEmpty());
 	}
 
 	@Test
 	public void testEmployeeDatabaseEmployeeDatabase() {
+		// Copying existing empty database
 		EmployeeDatabase firstBase = new EmployeeDatabase();
 		EmployeeDatabase secondBase = new EmployeeDatabase(firstBase);
 		Assert.assertTrue(secondBase.getDataBase().isEmpty());
 		
+		// Copying existing database
 		firstBase = new EmployeeDatabase();
 		firstBase.appendEmployee(new Employee("Knyazkin", "Max"));
 		secondBase = new EmployeeDatabase(firstBase);
-		Assert.assertTrue(!secondBase.getDataBase().isEmpty());
+		Assert.assertFalse(secondBase.getDataBase().isEmpty());
 	}
 
 	@Test
 	public void testEmployeeDatabaseListOfEmployee() {
-		fail("Not yet implemented");
+		List<Employee> list = new ArrayList<>();
+		list.add(new Employee("Knyazkin", "Max"));
+		list.add(new Employee("Tsydypov", "Agvan"));
+		list.add(new Employee("John", "Snow"));
+		
+		// Creating database from list
+		EmployeeDatabase database = new EmployeeDatabase(list);
+		Assert.assertEquals(list, database.getDataBase());
 	}
 
 	@Test
@@ -43,45 +55,134 @@ public class EmployeeDatabaseTest {
 		List<Employee> employees = database.getDataBase();
 		Employee employee = employees.get(0);
 		
-		Assert.assertTrue(employee.getSurname().equals("Knyazkin"));
-		Assert.assertTrue(employee.getName().equals("Max"));
+		// Adding employee to the database
+		Assert.assertEquals(employee.getSurname(), "Knyazkin");
+		Assert.assertEquals(employee.getName(), "Max");
 	}
 
 	@Test
 	public void testPrintAllEmployees() {
-		fail("Not yet implemented");
+		fail("Not implemented yet");
 	}
 
 	@Test
 	public void testGetDataBase() {
-		EmployeeDatabase database = new EmployeeDatabase();
-		database.appendEmployee(new Employee("Knyazkin", "Max"));
+		List<Employee> list = new ArrayList<>();
+		list.add(new Employee("Knyazkin", "Max"));
+		list.add(new Employee("Tsydypov", "Agvan"));
+		list.add(new Employee("John", "Snow"));
 		
-		List<Employee> employees = database.getDataBase();
-		Employee employee = employees.get(0);
-		
-		Assert.assertTrue(employee.getSurname().equals("Knyazkin"));
-		Assert.assertTrue(employee.getName().equals("Max"));
+		// Getting a list from a database
+		EmployeeDatabase database = new EmployeeDatabase(list);
+		Assert.assertEquals(list, database.getDataBase());
 	}
 
 	@Test
 	public void testFindEmployeeString() {
-		fail("Not yet implemented");
+		EmployeeDatabase database = new EmployeeDatabase();
+		Employee foundEmployee = database.findEmployee("John");
+		
+		// Finding employee in an empty database
+		Assert.assertTrue(foundEmployee instanceof NullEmployee);
+		
+		Employee employee = new Employee("Knyazkin", "Max");
+		database.appendEmployee(employee);
+		database.appendEmployee(new Employee("Tsydypov", "Agvan"));
+		database.appendEmployee(new Employee("John", "Snow"));
+		
+		foundEmployee = database.findEmployee("Knyazkin");
+		
+		// Finding employee
+		Assert.assertEquals(employee, foundEmployee);
+		
+		foundEmployee = database.findEmployee("Ackerman");
+		
+		// Trying to find employee that is not in a database
+		Assert.assertTrue(foundEmployee instanceof NullEmployee);
 	}
 
 	@Test
 	public void testFindEmployeeStringString() {
-		fail("Not yet implemented");
+		EmployeeDatabase database = new EmployeeDatabase();
+		Employee foundEmployee = database.findEmployee("John", "S");
+		
+		// Finding employee in an empty database
+		Assert.assertTrue(foundEmployee instanceof NullEmployee);
+		
+		Employee employee = new Employee("Knyazkin", "Max");
+		database.appendEmployee(employee);
+		database.appendEmployee(new Employee("Tsydypov", "Agvan"));
+		database.appendEmployee(new Employee("John", "Snow"));
+		
+		foundEmployee = database.findEmployee("Knyazkin", "Max");
+		
+		// Finding employee
+		Assert.assertEquals(employee, foundEmployee);
+		
+		foundEmployee = database.findEmployee("Ackerman", "Mikasa");
+		
+		// Trying to find employee that is not in a database
+		Assert.assertTrue(foundEmployee instanceof NullEmployee);
 	}
 
 	@Test
 	public void testFindEmployeesString() {
-		fail("Not yet implemented");
+		EmployeeDatabase database = new EmployeeDatabase();
+		
+		List<Employee> foundList = database.findEmployees("Knyazkin");
+		
+		// Trying to find employees that are not in a database
+		Assert.assertTrue(foundList.isEmpty());
+		
+		Employee employee = new Employee("Knyazkin", "Max");
+		database.appendEmployee(employee);
+		database.appendEmployee(new Employee("Tsydypov", "Agvan"));
+		database.appendEmployee(new Employee("John", "Snow"));
+		
+		List<Employee> list = new ArrayList<>();
+		list.add(employee);
+		
+		foundList = database.findEmployees("Knyazkin");
+
+		// Finding a list of employees
+		Assert.assertEquals(list, foundList);
+		
+		list.add(employee);
+		database = new EmployeeDatabase(list);
+		foundList = database.findEmployees("Knyazkin");
+		
+		// Finding a list of employees
+		Assert.assertEquals(list, foundList);
 	}
 
 	@Test
 	public void testFindEmployeesStringString() {
-		fail("Not yet implemented");
+EmployeeDatabase database = new EmployeeDatabase();
+		
+		List<Employee> foundList = database.findEmployees("Knyazkin", "Max");
+		
+		// Trying to find employees that are not in a database
+		Assert.assertTrue(foundList.isEmpty());
+		
+		Employee employee = new Employee("Knyazkin", "Max");
+		database.appendEmployee(employee);
+		database.appendEmployee(new Employee("Tsydypov", "Agvan"));
+		database.appendEmployee(new Employee("John", "Snow"));
+		
+		List<Employee> list = new ArrayList<>();
+		list.add(employee);
+		
+		foundList = database.findEmployees("Knyazkin", "Max");
+
+		// Finding a list of employees
+		Assert.assertEquals(list, foundList);
+		
+		list.add(employee);
+		database = new EmployeeDatabase(list);
+		foundList = database.findEmployees("Knyazkin", "Max");
+		
+		// Finding a list of employees
+		Assert.assertEquals(list, foundList);
 	}
 
 }
--- src/tests/EmployeeTest.java
+++ src/tests/EmployeeTest.java
@@ -6,43 +6,140 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import app.Employee;
+import app.NullEmployee;
 
 public class EmployeeTest {
 
 	@Test
 	public void testGetName() {
 		Employee employee = new Employee();
+		
+		// Creating an empty employee
 		Assert.assertTrue(employee.getName().equals(""));
 		
 		employee = new Employee("Knyazkin");
+		
+		// Creating an employee without name
 		Assert.assertTrue(employee.getName().equals(""));
 		
 		employee = new Employee("Knyazkin", "Max");
+		
+		// Creating an employee
 		Assert.assertTrue(employee.getName().equals("Max"));
 	}
 
 	@Test
 	public void testGetSurname() {
 		Employee employee = new Employee();
+		
+		// Creating an empty employee
 		Assert.assertTrue(employee.getSurname().equals(""));
 		
 		employee = new Employee("Knyazkin");
+		
+		// Creating an employee without name
 		Assert.assertTrue(employee.getSurname().equals("Knyazkin"));
 
 		employee = new Employee("Knyazkin", "Max");
+		
+		// Creating an employee
 		Assert.assertTrue(employee.getSurname().equals("Knyazkin"));
 	}
 
 	@Test
 	public void testGetSurnameAndName() {
 		Employee employee = new Employee();
+		
+		// Creating an empty employee
 		Assert.assertTrue(employee.getSurnameAndName().equals(" "));
 		
 		employee = new Employee("Knyazkin");
+		
+		// Creating an employee without name
 		Assert.assertTrue(employee.getSurnameAndName().equals("Knyazkin "));
 		
 		employee = new Employee("Knyazkin", "Max");
+		
+		// Creating an employee
 		Assert.assertTrue(employee.getSurnameAndName().equals("Knyazkin Max"));
 	}
-
+	
+	@Test
+	public void testEmployee() {
+		Employee employee = new Employee();
+		
+		// Creating an empty employee
+		Assert.assertEquals(employee.getSurname(), "");
+		Assert.assertEquals(employee.getName(), "");
+		Assert.assertEquals(employee.getSurnameAndName(), " ");
+	}
+	
+	@Test
+	public void testEmployeeString() {
+		Employee employee = new Employee("Knyazkin");
+		
+		// Creating an employee without name
+		Assert.assertEquals(employee.getSurname(), "Knyazkin");
+		Assert.assertEquals(employee.getName(), "");
+		Assert.assertEquals(employee.getSurnameAndName(), "Knyazkin ");
+	}
+	
+	@Test
+	public void testEmployeeStringString() {
+		Employee employee = new Employee("Snow", "John");
+		
+		// Creating an employee
+		Assert.assertEquals(employee.getSurname(), "Snow");
+		Assert.assertEquals(employee.getName(), "John");
+		Assert.assertEquals(employee.getSurnameAndName(), "Snow John");
+	}
+	
+	@Test
+	public void testEmployeeHashCode() {
+		Employee employee = new Employee("Snow", "John");
+		
+		// The same object
+		Assert.assertEquals(employee.hashCode(), employee.hashCode());
+		
+		Employee secondEmployee = new Employee("Snow", "John");
+		
+		// The equal object
+		Assert.assertEquals(employee.hashCode(), secondEmployee.hashCode());
+		
+		secondEmployee = new Employee("Snow", "Ramsay");
+		
+		// Different object
+		Assert.assertNotEquals(employee.hashCode(), secondEmployee.hashCode());
+		
+		secondEmployee = new NullEmployee();
+		
+		// Null employee
+		Assert.assertNotEquals(employee.hashCode(), secondEmployee.hashCode());
+	}
+	
+	@Test
+	public void testEmployeeEquals() {
+		Employee employee = new Employee("Snow", "John");
+		
+		// Null
+		Assert.assertFalse(employee.equals(null));
+		
+		// The same object
+		Assert.assertTrue(employee.equals(employee));
+		
+		Employee secondEmployee = new Employee("Snow", "John");
+		
+		// The equal object
+		Assert.assertTrue(employee.equals(secondEmployee));
+		
+		secondEmployee = new Employee("Snow", "Ramsay");
+		
+		// Different object
+		Assert.assertFalse(employee.equals(secondEmployee));
+		
+		secondEmployee = new NullEmployee();
+		
+		// Null employee
+		Assert.assertFalse(employee.equals(secondEmployee));
+	}
 }
